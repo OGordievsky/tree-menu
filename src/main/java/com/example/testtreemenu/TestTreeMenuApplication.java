3e36f4185fea824cd5d49701f884831a91ca251c
Oleg <og87@bk.ru>
2022-10-08 11:56:33 +0300

    init: start

---
+++ src/main/java/com/example/testtreemenu/TestTreeMenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtreemenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTreeMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTreeMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testtreemenu/TestTreeMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtreemenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTreeMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
